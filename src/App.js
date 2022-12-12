cbb66f4f9dd1554c94c4c225f679864d8ccafaf3
Todu66 <giga.todua06@gmail.com>
2022-12-12 12:40:13 +0400

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import data from "./data.json"
+console.log(data)
+export default function App () {
+  const card = data.map(item => {
+    return (
+      <Header 
+        name={item.name}
+        temperature={item.temperature}
+      />
+    )
+    
+  })
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {card}
     </div>
-  );
+  )
 }
-
-export default App;
