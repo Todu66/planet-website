c6038927d184005d794b15569c3ea3ed04321c7a
Todu66 <giga.todua06@gmail.com>
2023-01-02 23:06:32 +0400

    some changes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./Hero.css";
-import data from "../../data.json";
 
-console.log(data);
+import Mercury from "../pages/Mercury"
+
+
 export default function Hero() {
-  const mapingImages = data.map((item) => {
-    return <img className="image1" src={item.images.planet} />;
-  });
-  return (
-    <div className="both-side">
-      <div className="left-side-img">
-        {mapingImages}
-        </div>
+  <>
+    <Mercury />
+    
+  </>
+  
 
-      <div className="right-side">
-      </div>
-    </div>
-  );
 }
