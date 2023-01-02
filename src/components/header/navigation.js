893918cf54f9937997d4131ae33c40257d1a39b8
Todu66 <giga.todua06@gmail.com>
2023-01-02 23:12:18 +0400

    nothing

---
+++ src/components/header/navigation.js
@@ -0,0 +1,23 @@
+// import planetData from "../../data.json";
+// import Header from "./Header";
+// import "../css/Header.css";
+// import React, { useState } from "react";
+
+// export default function Navigation() {
+//   const changedData = planetData.map((planet) => (
+//     <a onClick={() => handlePlanetClick(planet)}>{planet.name}</a>
+//   ));
+
+
+//   const [currentPlanet, setCurrentPlanet] = useState(null);
+
+
+//   function handlePlanetClick(planet) {
+//     setCurrentPlanet(planet);    
+//   }
+
+
+//   return (
+
+//   );
+// }
